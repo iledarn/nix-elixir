4d36c73ba7aa09837b9334b97db58e282db5db30
iledarn <iledarn@gmail.com>
2022-04-28 03:11:41 +0300

    ref - bird-count: concise, use pattern matching, guard clauses

--- elixir/bird-count/lib/bird_count.ex
+++ elixir/bird-count/lib/bird_count.ex
@@ -1,47 +1,18 @@
 defmodule BirdCount do
-  def today([]) do
-    nil
-  end
+  def today([]), do: nil
+  def today([head | _tail]), do: head
 
-  def today([head | _tail]) do
-    head
-  end
+  def increment_day_count([]), do: [1]
+  def increment_day_count([head | tail]), do: [head + 1 | tail]
 
-  def increment_day_count([]) do
-    [1]
-  end
+  def has_day_without_birds?([]), do: false
+  def has_day_without_birds?([0 | tail]), do: true
+  def has_day_without_birds?([_ | tail]), do: has_day_without_birds?(tail)
 
-  def increment_day_count([head | tail]) do
-    [head + 1 | tail]
-  end
+  def total([]), do: 0
+  def total([head | tail]), do: total(tail) + head
 
-  def has_day_without_birds?([]) do
-    false
-  end
-
-  def has_day_without_birds?([head | tail]) do
-    case head == 0 do
-      true -> true
-      _ -> has_day_without_birds?(tail) == true
-    end
-  end
-
-  def total([]) do
-    0
-  end
-
-  def total([head | tail]) do
-    total(tail) + head
-  end
-
-  def busy_days([]) do
-    0
-  end
-
-  def busy_days([head | tail]) do
-    case head >= 5 do
-      true -> 1 + busy_days(tail)
-      _ -> busy_days(tail)
-    end
-  end
+  def busy_days([]), do: 0
+  def busy_days([head | tail]) when head >= 5, do: 1 + busy_days(tail)
+  def busy_days([_ | tail]), do: busy_days(tail)
 end
