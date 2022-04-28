598d19dc254ef0da203bef173df6374ce0184e2b
iledarn <iledarn@gmail.com>
2022-04-28 03:13:05 +0300

    fix - bird-count: variable tail is unused

--- elixir/bird-count/lib/bird_count.ex
+++ elixir/bird-count/lib/bird_count.ex
@@ -6,7 +6,7 @@ defmodule BirdCount do
   def increment_day_count([head | tail]), do: [head + 1 | tail]
 
   def has_day_without_birds?([]), do: false
-  def has_day_without_birds?([0 | tail]), do: true
+  def has_day_without_birds?([0 | _]), do: true
   def has_day_without_birds?([_ | tail]), do: has_day_without_birds?(tail)
 
   def total([]), do: 0
