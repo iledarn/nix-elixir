7c040abfe6964e8441e368fa83c3ed6b9ef70ec0
iledarn <iledarn@gmail.com>
2022-05-03 18:13:35 +0300

    name-badge: task 3

--- elixir/name-badge/lib/name_badge.ex
+++ elixir/name-badge/lib/name_badge.ex
@@ -1,9 +1,17 @@
 defmodule NameBadge do
   def print(id, name, department) do
     if id do
-      "[#{id}] - #{name} - #{String.upcase(department)}"
+      if department do
+        "[#{id}] - #{name} - #{String.upcase(department)}"
+      else
+        "[#{id}] - #{name} - OWNER"
+      end
     else
-      "#{name} - #{String.upcase(department)}"
+      if department do
+        "#{name} - #{String.upcase(department)}"
+      else
+        "#{name} - OWNER"
+      end
     end
   end
 end
